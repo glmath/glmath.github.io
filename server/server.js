086449ed3edf0dba02fc7c77c9fe8b78d404643d
Shahan Neda <shahan.neda@gmail.com>
2020-10-16 09:54:55 -0700

    moved over to env for port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 7772;
+const port = process.env.PORT;
 const { exec } = require("child_process");
 
 const mongo = require("mongodb");
