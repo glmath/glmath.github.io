265aa2d58e94706d48885d5f2391c60fb85ce204
shahanneda <shahan.neda@gmail.com>
2020-10-29 22:53:34 -0700

    added new password modal and login

--- client/src/js/LMath.jsx
+++ client/src/js/LMath.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Container, Button } from "react-bootstrap";
+import React, { Component, useState } from "react";
+import { Container, Button, Modal, Spinner, Form } from "react-bootstrap";
 import {
   HashRouter,
   Switch,
@@ -17,6 +17,7 @@ import Lesson from "./Lesson.jsx"
 import LessonBrowser from "./LessonBrowser.jsx"
 
 import Cookies from 'universal-cookie';
+import loader from "sass-loader";
 const cookies = new Cookies();
 
 class LMath extends Component {
@@ -30,10 +31,23 @@ class LMath extends Component {
     this.state = {
       isAdmin: admin,
       sessionId: sessionId,
+      showingModal: false,
+      modalType: "loading",
+      shouldShowCloseButton: true,
+      closeButtonText: "Cancel",
+      modalContent: "",
     }
   }
   loginButtonClicked = () => {
-    let password = prompt("Enter password");
+    this.setState({ showingModal: true, modalType: "login" });
+
+
+
+  }
+
+  loginModalSubmit = (password) => {
+    this.setState({ modalType: "loading", shouldShowCloseButton: true, closeButtonText: "Cancel" })
+
 
     fetch(this.props.url + "/login", {
       method: 'POST',
@@ -47,16 +61,16 @@ class LMath extends Component {
     }).then(res => res.json())
       .then(res => {
         console.log(res);
-        if(res.status == "success"){
-          this.setState({isAdmin:true, sessionId: res.sessionId});
+        if (res.status == "success") {
+          this.setState({ isAdmin: true, sessionId: res.sessionId, showingModal: false });
           cookies.set("isAdmin", "true");
           cookies.set("sessionId", res.sessionId);
-        }else{
-          alert("Incorrect password!");
+        } else {
+          this.setState({ modalType: "content", modalContent: "Incorrect Password!", shouldShowCloseButton: true, closeButtonText: "Close" })
         }
       });
-  }
 
+  }
   logoutButtonClicked = () => {
     fetch(this.props.url + "/logout", {
       method: 'GET',
@@ -68,16 +82,26 @@ class LMath extends Component {
       .then(res => {
         console.log(res);
       });
-      this.setState({isAdmin: false});
-      cookies.set("isAdmin", "false")
+    this.setState({ isAdmin: false });
+    cookies.set("isAdmin", "false")
 
-      location.reload();
+    location.reload();
   }
 
   render() {
     return (
-      <Container fluid>
-
+      <Container fluid> {this.state.showingModal ?
+        <LoginModal
+          type={this.state.modalType}
+          content={this.state.modalContent}
+          closeButton={true}
+          onSubmit={this.loginModalSubmit}
+          close={() => this.setState({ showingModal: false })}
+          shouldShowCloseButton={this.state.shouldShowCloseButton}
+          closeButtonText={this.state.closeButtonText}
+        />
+        : ""
+      }
 
         <HashRouter>
           <Switch>
@@ -85,7 +109,7 @@ class LMath extends Component {
 
             <Route path="/math/:id" children={
               <div className="lesson-page-wrapper row">
-                <LessonBrowser sessionId={this.state.sessionId} defaultCollapsed={false}  loginButton={this.loginButtonClicked} logoutButton={this.logoutButtonClicked} className="col-3" url={this.props.url} clientUrl={this.props.clientUrl} isAdmin={this.state.isAdmin} />
+                <LessonBrowser sessionId={this.state.sessionId} defaultCollapsed={false} loginButton={this.loginButtonClicked} logoutButton={this.logoutButtonClicked} className="col-3" url={this.props.url} clientUrl={this.props.clientUrl} isAdmin={this.state.isAdmin} />
                 <LessonLoader sessionId={this.state.sessionId} loginButton={this.loginButtonClicked} logoutButton={this.logoutButtonClicked} isAdmin={this.state.isAdmin} url={this.props.url} clientUrl={this.props.clientUrl} />
               </div>
             }></Route>
@@ -123,8 +147,61 @@ function LessonLoader(props) {
 
   return (
     <div className="lesson-loader-wrapper col-9">
-      <Lesson  sessionId={props.sessionId} loginButton={props.loginButton} logoutButton={props.logoutButton} key={id} id={id} url={props.url} isAdmin={props.isAdmin} clientUrl={props.clientUrl} />
+      <Lesson sessionId={props.sessionId} loginButton={props.loginButton} logoutButton={props.logoutButton} key={id} id={id} url={props.url} isAdmin={props.isAdmin} clientUrl={props.clientUrl} />
     </div>
+  );
+}
+
+function LoginModal({ close, onSubmit, content, type = "loading", shouldShowCloseButton, closeButtonText = "Cancel" }) {
+
+  const handleClose = () => close();
+
+  const [password, setPassword] = useState("");
+
+
+  return (
+
+    <Modal show={true} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title></Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {type == "loading" ? <Spinner className="spinner spinner-sm" animation="border" role="status">
+          <span className="sr-only">Starting up server, this might take a while the first time...</span>
+        </Spinner> :
+          content
+        }
+        {type == "login" ?
+
+          <Form>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
+            </Form.Group>
+          </Form>
+
+
+
+
+          : ""}
+
+      </Modal.Body>
+
+
+
+      <Modal.Footer>
+        {shouldShowCloseButton ?
+          <Button variant="secondary" onClick={handleClose}>
+            {closeButtonText}
+          </Button> : ""}
+
+
+        {type == "login" ?
+          <Button variant="primary" onClick={() => onSubmit(password)}>
+            Login
+                </Button> : ""}
+
+      </Modal.Footer>
+    </Modal>
   );
 }
 
