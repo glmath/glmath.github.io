cc1f44a606a1c8ccfef2e79a9030a9999d8e7d4f
shahanneda <shahan.neda@gmail.com>
2020-11-08 19:16:15 -0800

    added limits to shortcuts

--- client/src/js/Lesson.jsx
+++ client/src/js/Lesson.jsx
@@ -77,7 +77,7 @@ class Lesson extends Component {
       enableMathQuillFormulaAuthoring(this.reactQuill.current.editor, {
         operators: [["\\pm", "\\pm"], ["\\sqrt{x}", "\\sqrt"], ["\\sqrt[n]{x}", "\\nthroot"], ["\\frac{x}{y}", "\\frac"],
         ["\\sum^{s}_{x}{d}", "\\sum"], ["\\prod^{s}_{x}{d}", "\\prod"], ["\\coprod^{s}_{x}{d}", "\\coprod"],
-        ["\\int^{s}_{x}{d}", "\\int"], ["\\binom{n}{k}", "\\binom"]],
+        ["\\int^{s}_{x}{d}", "\\int"], ["\\binom{n}{k}", "\\binom"], ["\\lim_{k→n}", "\\lim"]],
 
 
         displayHistory: true,
