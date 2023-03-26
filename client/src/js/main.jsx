41bc679db58d38e845243309c8089dbcb8569145
shahanneda <shahan.neda@gmail.com>
2023-03-25 21:20:24 -0400

    Updated server url

--- client/src/js/main.jsx
+++ client/src/js/main.jsx
@@ -5,11 +5,12 @@ import LMath from "./LMath.jsx";
 const wrapper = document.getElementById("container");
 
 ReactDOM.render(
-        <div>
-          <LMath url="https://glmathserver.xyz" clientUrl={"https://glmath.github.io/client"} />
-          {/* <LMath url="" clientUrl={"https://glmath.github.io/client"} ad/> */}
-        </div>
-, wrapper);
-
-
-
+  <div>
+    <LMath
+      url="https://shahanneda.xyz/glmath"
+      clientUrl={"https://glmath.github.io/client"}
+    />
+    {/* <LMath url="" clientUrl={"https://glmath.github.io/client"} ad/> */}
+  </div>,
+  wrapper
+);
